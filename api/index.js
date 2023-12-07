5d7138055598efe024f9c02ca9b2c4c435716154
Amene Terhemen <ameneterh@gmail.com>
2023-12-07 16:44:59 +0100

    create test api route

--- api/index.js
+++ api/index.js
@@ -16,11 +16,8 @@ mongoose
 
 const app = express();
 
-app.listen(
-  (3000,
-  () => {
-    console.log("Server is running on port 3000");
-  })
-);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
 app.use("/api/user", userRouter);
