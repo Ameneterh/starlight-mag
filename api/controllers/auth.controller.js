6b5c492ec5357b2adbcf33c50f3fd8f6179f3859
Amene Terhemen <ameneterh@gmail.com>
2023-12-08 19:14:22 +0100

    complete signup functionality and create signin

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import { response } from "express";
 import User from "../models/user.models.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const {
@@ -32,6 +34,31 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ username });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) return next(errorHandler(404, "Wrong credentials!"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
