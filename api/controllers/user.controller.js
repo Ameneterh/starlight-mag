a287b13dec250b6e05d8dc5d6b0acbbf311fe1eb
Amene Terhemen <ameneterh@gmail.com>
2023-12-10 16:49:03 +0100

    create update user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,38 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/user.models.js";
+
 export const test = (req, res) => {
   res.json({ message: "Hello from Controller!" });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only update your own account!"));
+
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          authorName: req.body.authorName,
+          email: req.body.email,
+          phoneNumber: req.body.phoneNumber,
+          socialMedia: req.body.socialMedia,
+          avatar: req.body.avatar,
+          role: req.body.role,
+          aboutAuthor: req.body.aboutAuthor,
+          username: req.body.username,
+          password: req.body.password,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
