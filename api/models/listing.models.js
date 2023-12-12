c010c793ab07f19e9ba71756b1d25a832bad1bbb
Amene Terhemen <ameneterh@gmail.com>
2023-12-12 22:25:59 +0100

    create article listing api route

--- api/models/listing.models.js
+++ api/models/listing.models.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const listingSchema = new mongoose.Schema(
   {
     imageUrl: {
-      type: image,
+      type: Array,
       required: true,
     },
     title: {
