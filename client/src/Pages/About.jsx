5bb02585394b7d542e35893bf62c70d0ee351edf
Amene Terhemen <ameneterh@gmail.com>
2023-12-17 17:41:36 +0100

    adjust input fields to add label

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function About() {
-  return <div className="h-screen w-full">About Startlight Mag</div>;
+  return (
+    <div className="h-screen w-full py-20 px-4 max-w-7xl">
+      About Startlight Mag
+    </div>
+  );
 }
